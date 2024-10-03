c29b8bc6576a8992661c35f9ad962df266f24b3e
dev bruno ciccio <dev.bruno.ciccio@gmail.com>
2024-10-03 09:32:04 -0300

    upload primeiros arquivo do projeto chat-message

---
+++ src/test/java/br/com/chatmessage/ChatmessageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chatmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
